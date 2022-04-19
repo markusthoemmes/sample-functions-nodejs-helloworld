099216aa24ab05a2df82b8f0837658b9b3798dc7
Markus Thömmes <mthoemmes@digitalocean.com>
2022-04-19 08:53:57 +0200

    More logs

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -2,6 +2,7 @@ function main(args) {
     let name = args.name || 'stranger'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
+    console.log(process.env.LOG_DESTINATIONS)
     return {"body": greeting}
   }
 
